b4c4795f4eb3d1d42648f1dab6bf783c3e91957c
Puneeth K <kanike.puneeth@gmail.com>
2024-10-21 19:19:38 +0530

    change 1

--- components/Banner.tsx
+++ components/Banner.tsx
@@ -1,40 +1,55 @@
 import { motion } from "framer-motion";
+import { Typewriter } from "react-simple-typewriter"; // Import Typewriter
+
 const Banner = () => {
   return (
     <section
       id="home"
-      className="max-w-contentContainer mx-auto py-10 mdl:py-24 flex flex-col gap-4 lgl:gap-8 mdl:px-10 xl:px-4"
+      className="max-w-contentContainer mx-auto py-10 mdl:py-28 flex flex-col gap-4 lgl:gap-8 mdl:px-10 xl:px-4"
     >
       
       <motion.h1
         initial={{ y: 10, opacity: 0 }}
         animate={{ y: 0, opacity: 1 }}
         transition={{ duration: 0.5, delay: 0.7 }}
-        className="text-4xl lgl:text-5xl font-titleFont font-semibold flex flex-col"
+        className="text-4xl text-textDark lgl:text-5xl font-titleFont font-semibold flex flex-col"
       >
         Hi There,
-        </motion.h1>
-        <motion.h1
+      </motion.h1>
+      
+      <motion.h1
         initial={{ y: 10, opacity: 0 }}
         animate={{ y: 0, opacity: 1 }}
         transition={{ duration: 0.5, delay: 0.7 }}
         className="text-4xl lgl:text-6xl font-titleFont font-semibold flex flex-col"
       >
-        <span className="text-textDark">
-  {"I'm Mohammed"} <span className="text-textGreen">Safwan</span>.
-</span>
-
+        <span>
+          {"I'm Mohammed"} <span className="text-textGreen">Safwan</span>.
+        </span>
       </motion.h1>
+      
       <motion.p
         initial={{ y: 10, opacity: 0 }}
         animate={{ y: 0, opacity: 1 }}
         transition={{ duration: 0.5, delay: 0.8 }}
-        className="text-base md:max-w-[650px] text-textDark font-medium"
+        className="text-2xl md:max-w-[650px] text-textDark font-medium"
       >
-        I vision into reality!{" "}
-        
+        I Am Into{" "}
+        <span className="text-textGreen font-bold">
+          {/* Implementing Typewriter effect here */}
+          <Typewriter
+            words={["Interior Designing", "3D Visualizing", "Space Planning", "Design Managing"]}
+            loop={true} // Loop through words
+            cursor
+            cursorStyle="|" // Style of the cursor
+            typeSpeed={70} // Typing speed
+            deleteSpeed={50} // Deleting speed
+            delaySpeed={1000} // Delay before changing to next word
+          />
+        </span>
       </motion.p>
-      <a href="https://github.com/PuneethKanike" target="_blank">
+      
+      <a href="https://github.com/PuneethKanike" target="_blank" rel="noopener noreferrer">
         <motion.button
           initial={{ y: 10, opacity: 0 }}
           animate={{ y: 0, opacity: 1 }}
