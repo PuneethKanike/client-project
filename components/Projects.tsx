a6f69880b08c6245d1a095c6f8863664b0be0a43
Puneeth K <kanike.puneeth@gmail.com>
2024-10-24 21:18:33 +0530

    change 24

--- components/Projects.tsx
+++ components/Projects.tsx
@@ -3,13 +3,56 @@ import { eShop, jobJunction, realEstate } from "@/public/assets";
 import Image from "next/image";
 import SectionTitle from "./SectionTitle";
 import CarouselModal from "./CarouselModal"; // Import the modal component
-import { StaticImageData } from 'next/image';
+import { StaticImageData } from "next/image";
 
 import { p1i1 } from "@/public/assets";
 import { p1i2 } from "@/public/assets";
 import { p1i3 } from "@/public/assets";
 import { p1i4 } from "@/public/assets";
 import { p1i5 } from "@/public/assets";
+import { p1i6 } from "@/public/assets";
+import { p1i7 } from "@/public/assets";
+import { p1i8 } from "@/public/assets";
+import { p1i9 } from "@/public/assets";
+import { p1i10 } from "@/public/assets";
+import { p1i11 } from "@/public/assets";
+
+//project2
+import { p2i1 } from "@/public/assets";
+import { p2i2 } from "@/public/assets";
+import { p2i3 } from "@/public/assets";
+import { p2i4 } from "@/public/assets";
+import { p2i5 } from "@/public/assets";
+import { p2i6 } from "@/public/assets";
+import { p2i7 } from "@/public/assets";
+import { p2i8 } from "@/public/assets";
+import { p2i9 } from "@/public/assets";
+
+//project3
+import { p3i1 } from "@/public/assets";
+import { p3i2 } from "@/public/assets";
+import { p3i3 } from "@/public/assets";
+import { p3i4 } from "@/public/assets";
+import { p3i5 } from "@/public/assets";
+import { p3i6 } from "@/public/assets";
+import { p3i7 } from "@/public/assets";
+import { p3i8 } from "@/public/assets";
+
+//project4
+import { p4i1 } from "@/public/assets";
+import { p4i2 } from "@/public/assets";
+import { p4i3 } from "@/public/assets";
+import { p4i4 } from "@/public/assets";
+import { p4i5 } from "@/public/assets";
+import { p4i6 } from "@/public/assets";
+import { p4i7 } from "@/public/assets";
+import { p4i8 } from "@/public/assets";
+import { p4i9 } from "@/public/assets";
+import { p4i10 } from "@/public/assets";
+import { p4i11 } from "@/public/assets";
+import { p4i12 } from "@/public/assets";
+import { p4i13 } from "@/public/assets";
+import { p4i14 } from "@/public/assets";
 
 const Projects = () => {
   const [showModal, setShowModal] = useState(false);
@@ -17,14 +60,55 @@ const Projects = () => {
   const [currentImages, setCurrentImages] = useState<string[]>([]);
 
   // Each project can have its own set of related images
-  const projectOneImages = [p1i1, p1i2, p1i3, p1i4, p1i5];
-  const projectTwoImages = [jobJunction, "/path/to/relatedImage1.jpg", "/path/to/relatedImage2.jpg"];
-  const projectThreeImages = [realEstate, "/path/to/relatedImage1.jpg", "/path/to/relatedImage2.jpg"];
+  const projectOneImages = [
+    p1i11,
+    p1i1,
+    p1i2,
+    p1i3,
+    p1i4,
+    p1i5,
+    p1i6,
+    p1i7,
+    p1i8,
+    p1i9,
+    p1i10,
+  ];
+  const projectTwoImages = [
+    p2i1,
+    p2i2,
+    p2i3,
+    p2i4,
+    p2i5,
+    p2i6,
+    p2i7,
+    p2i8,
+    p2i9,
+  ];
+  const projectThreeImages = [p3i1, p3i2, p3i3, p3i4, p3i5, p3i6, p3i7, p3i8];
+  const projectFourImages = [
+    p4i14,
+    p4i1,
+    p4i2,
+    p4i3,
+    p4i4,
+    p4i5,
+    p4i6,
+    p4i7,
+    p4i8,
+    p4i9,
+    p4i10,
+    p4i11,
+    p4i12,
+    p4i13,
+  ];
 
   // Handle image click
-  const handleImageClick = (images: (string | StaticImageData)[], index: number) => {
+  const handleImageClick = (
+    images: (string | StaticImageData)[],
+    index: number
+  ) => {
     const imageUrls = images.map((image) =>
-      typeof image === 'string' ? image : image.src
+      typeof image === "string" ? image : image.src
     );
     setCurrentImages(imageUrls); // Set the converted URLs
     setSelectedImageIndex(index);
@@ -36,88 +120,160 @@ const Projects = () => {
   };
 
   return (
-    <section id="project" className="relative max-w-container mx-auto lgl:px-20 py-24">
+    <section
+      id='project'
+      className='relative max-w-container mx-auto lgl:px-20 py-24'
+    >
       {/* Content */}
-      <div className="relative z-10">
-        <SectionTitle title="Some Things I have Designed" titleNo="03" />
-        <div className="w-full flex flex-col items-center justify-center gap-28 mt-10">
+      <div className='relative z-10'>
+        <SectionTitle title='Some Things I have Designed' titleNo='03' />
+        <div className='w-full flex flex-col items-center justify-center gap-28 mt-10'>
           {/* Project One */}
-          <div className="flex flex-col xl:flex-row gap-6">
+          <div className='flex flex-col xl:flex-row gap-6'>
             <div
-              className="w-full xl:w-96 h-auto relative group"
+              className='w-full xl:w-96 h-auto relative group'
               onClick={() => handleImageClick(projectOneImages, 0)}
             >
-              <div className="w-full h-72 overflow-hidden rounded-lg">
+              <div className='w-full h-72 overflow-hidden rounded-lg'>
                 <Image
-                  className="container-left rounded-xl transform transition duration-300 hover:scale-105 hover:shadow-xl object-cover w-full h-full cursor-pointer"
-                  src={p1i1}
-                  alt="eShop"
-                  layout="fill"
+                  className='container-left rounded-xl transform transition duration-300 hover:scale-105 hover:shadow-xl object-cover w-full h-full cursor-pointer'
+                  src={p1i11}
+                  alt='eShop'
+                  layout='fill'
                 />
               </div>
             </div>
-            <div className="w-full xl:w-1/2 flex flex-col gap-6 lgl:justify-start items-end text-right xl:-ml-16 z-10">
+            <div className='w-full xl:w-1/2 flex flex-col gap-6 lgl:justify-start items-end text-right xl:-ml-16 z-10'>
               <div>
-                <p className="font-titleFont text-textGreen text-sm tracking-wide">Featured Design</p>
-                <h3 className="container-right text-2xl font-bold">Forever - (An E-Commerce Web App)</h3>
+                <p className='font-titleFont text-textGreen text-sm tracking-wide'>
+                  Featured Design
+                </p>
+                <h3 className='container-right text-2xl font-bold'>Design 1</h3>
               </div>
-              <p className="container-right bg-[#b090e7] text-sm text-slate-900 md:text-lg p-2 md:p-6 rounded-md">
-                Programmed a full-stack e-commerce website allowing users to browse products, filter by categories, add
-                items to a cart, and complete purchases with COD, Stripe, or Razorpay.
+              <p className='container-right bg-[#b090e7] text-sm text-slate-900 md:text-lg p-2 md:p-6 rounded-md'>
+                Programmed a full-stack e-commerce website allowing users to
+                browse products, filter by categories, add items to a cart, and
+                complete purchases with COD, Stripe, or Razorpay.
               </p>
             </div>
           </div>
 
           {/* Project Two */}
-          <div className="flex flex-col xl:flex-row-reverse gap-6">
+          <div className='flex flex-col xl:flex-row-reverse gap-6'>
             <div
-              className="w-full xl:w-96  h-auto relative group"
+              className='w-full xl:w-96  h-auto relative group'
               onClick={() => handleImageClick(projectTwoImages, 0)}
             >
-              <div className="w-full h-72 overflow-hidden rounded-lg">
+              <div className='w-full h-72 overflow-hidden rounded-lg'>
                 <Image
-                  className="rounded-xl container-right transform transition duration-300 hover:scale-105 hover:shadow-xl object-cover w-full h-full cursor-pointer"
-                  src={jobJunction}
-                  alt="Job Junction"
-                  layout="fill"
+                  className='rounded-xl container-right transform transition duration-300 hover:scale-105 hover:shadow-xl object-cover w-full h-full cursor-pointer'
+                  src={p2i1}
+                  alt='Job Junction'
+                  layout='fill'
                 />
               </div>
             </div>
-            <div className="w-full xl:w-1/2 flex flex-col gap-6 justify-start items-start text-left z-10">
+            <div className='w-full xl:w-1/2 flex flex-col gap-6 justify-start items-start text-left z-10'>
               <div>
-                <p className="font-titleFont text-textGreen text-sm tracking-wide">Featured Design</p>
-                <h3 className="container-right text-2xl font-bold">Job Junction (Job Portal Web App)</h3>
+                <p className='font-titleFont text-textGreen text-sm tracking-wide'>
+                  Featured Design
+                </p>
+                <h3 className='container-right text-2xl font-bold'>Design 2</h3>
               </div>
-              <p className="xl:-mr-16  container-left bg-[#b090e7] text-sm text-slate-900 md:text-lg p-2 md:p-6 rounded-md">
-                Engineered a full-stack job portal website that allows users to browse job listings, search for jobs, apply for
-                positions, and provides an employer dashboard for managing job applications.
+              <p className='xl:-mr-16  container-left bg-[#b090e7] text-sm text-slate-900 md:text-lg p-2 md:p-6 rounded-md'>
+                Engineered a full-stack job portal website that allows users to
+                browse job listings, search for jobs, apply for positions, and
+                provides an employer dashboard for managing job applications.
               </p>
             </div>
           </div>
 
           {/* Project Three */}
-          <div className="flex flex-col xl:flex-row gap-6">
+          <div className='flex flex-col xl:flex-row gap-6'>
             <div
-              className="w-full xl:w-96  h-auto relative group"
+              className='w-full xl:w-96  h-auto relative group'
               onClick={() => handleImageClick(projectThreeImages, 0)}
             >
-              <div className="w-full h-72 overflow-hidden rounded-lg">
+              <div className='w-full h-72 overflow-hidden rounded-lg'>
                 <Image
-                  className= "container-left rounded-xl transform transition duration-300 hover:scale-105 hover:shadow-xl object-cover w-full h-full cursor-pointer"
-                  src={realEstate}
-                  alt="Real Estate"
-                  layout="fill"
+                  className='container-left rounded-xl transform transition duration-300 hover:scale-105 hover:shadow-xl object-cover w-full h-full cursor-pointer'
+                  src={p3i1}
+                  alt='Real Estate'
+                  layout='fill'
                 />
               </div>
             </div>
-            <div className="w-full xl:w-1/2 flex flex-col gap-6 justify-start items-end text-right xl:-ml-16 z-10">
+            <div className='w-full xl:w-1/2 flex flex-col gap-6 justify-start items-end text-right xl:-ml-16 z-10'>
               <div>
-                <p className="font-titleFont text-textGreen text-sm tracking-wide">Featured Design</p>
-                <h3 className="container-left text-2xl font-bold">LiveLink (Real-Estate Web App)</h3>
+                <p className='font-titleFont text-textGreen text-sm tracking-wide'>
+                  Featured Design
+                </p>
+                <h3 className='container-left text-2xl font-bold'>Design 3</h3>
               </div>
-              <p className="container-right bg-[#b090e7] text-sm text-slate-900 md:text-lg p-2 md:p-6 rounded-md">
-                Created a comprehensive real estate platform where users can register, manage profiles, list properties,
-                perform advanced searches, and contact landlords directly.
+              <p className='container-right bg-[#b090e7] text-sm text-slate-900 md:text-lg p-2 md:p-6 rounded-md'>
+                Created a comprehensive real estate platform where users can
+                register, manage profiles, list properties, perform advanced
+                searches, and contact landlords directly.
+              </p>
+            </div>
+          </div>
+
+          {/* project 4 */}
+          <div className='flex flex-col xl:flex-row-reverse gap-6'>
+            <div
+              className='w-full xl:w-96  h-auto relative group'
+              onClick={() => handleImageClick(projectFourImages, 0)}
+            >
+              <div className='w-full h-72 overflow-hidden rounded-lg'>
+                <Image
+                  className='rounded-xl container-right transform transition duration-300 hover:scale-105 hover:shadow-xl object-cover w-full h-full cursor-pointer'
+                  src={p4i14}
+                  alt='Job Junction'
+                  layout='fill'
+                />
+              </div>
+            </div>
+            <div className='w-full xl:w-1/2 flex flex-col gap-6 justify-start items-start text-left z-10'>
+              <div>
+                <p className='font-titleFont text-textGreen text-sm tracking-wide'>
+                  Featured Design
+                </p>
+                <h3 className='container-right text-2xl font-bold'>Design 4</h3>
+              </div>
+              <p className='xl:-mr-16  container-left bg-[#b090e7] text-sm text-slate-900 md:text-lg p-2 md:p-6 rounded-md'>
+                Engineered a full-stack job portal website that allows users to
+                browse job listings, search for jobs, apply for positions, and
+                provides an employer dashboard for managing job applications.
+              </p>
+            </div>
+          </div>
+
+          {/* project 5 */}
+          <div className='flex flex-col xl:flex-row gap-6'>
+            <div
+              className='w-full xl:w-96  h-auto relative group'
+              onClick={() => handleImageClick(projectThreeImages, 0)}
+            >
+              <div className='w-full h-72 overflow-hidden rounded-lg'>
+                <Image
+                  className='container-left rounded-xl transform transition duration-300 hover:scale-105 hover:shadow-xl object-cover w-full h-full cursor-pointer'
+                  src={p3i1}
+                  alt='Real Estate'
+                  layout='fill'
+                />
+              </div>
+            </div>
+            <div className='w-full xl:w-1/2 flex flex-col gap-6 justify-start items-end text-right xl:-ml-16 z-10'>
+              <div>
+                <p className='font-titleFont text-textGreen text-sm tracking-wide'>
+                  Featured Design
+                </p>
+                <h3 className='container-left text-2xl font-bold'>Design 5</h3>
+              </div>
+              <p className='container-right bg-[#b090e7] text-sm text-slate-900 md:text-lg p-2 md:p-6 rounded-md'>
+                Created a comprehensive real estate platform where users can
+                register, manage profiles, list properties, perform advanced
+                searches, and contact landlords directly.
               </p>
             </div>
           </div>
@@ -126,7 +282,11 @@ const Projects = () => {
 
       {/* Modal */}
       {showModal && (
-        <CarouselModal images={currentImages} initialIndex={selectedImageIndex} onClose={closeModal} />
+        <CarouselModal
+          images={currentImages}
+          initialIndex={selectedImageIndex}
+          onClose={closeModal}
+        />
       )}
     </section>
   );
--- public/assets/index.ts
+++ public/assets/index.ts
@@ -13,7 +13,66 @@ import p1i2 from "./images/projects/project1/image2.jpg"
 import p1i3 from "./images/projects/project1/image3.jpg"
 import p1i4 from "./images/projects/project1/image4.jpg"
 import p1i5 from "./images/projects/project1/image5.jpg"
+import p1i6 from "./images/projects/project1/image6.jpg"
+import p1i7 from "./images/projects/project1/image7.jpg"
+import p1i8 from "./images/projects/project1/image8.jpg"
+import p1i9 from "./images/projects/project1/image9.jpg"
+import p1i10 from "./images/projects/project1/image10.jpg"
+import p1i11 from "./images/projects/project1/image11.jpg"
 
-export { eShop, logo, profileImg, amazonImg, cyberImg, noorShop, jobJunction, realEstate, bg, bg2 
-, p1i1, p1i2, p1i3, p1i4, p1i5
+
+//project2'
+
+import p2i1 from "./images/projects/project2/image1.jpg"
+import p2i2 from "./images/projects/project2/image2.jpg"
+import p2i3 from "./images/projects/project2/image3.jpg"
+import p2i4 from "./images/projects/project2/image4.jpg"
+import p2i5 from "./images/projects/project2/image5.jpg"
+import p2i6 from "./images/projects/project2/image6.jpg"
+import p2i7 from "./images/projects/project2/image7.jpg"
+import p2i8 from "./images/projects/project2/image8.jpg"
+import p2i9 from "./images/projects/project2/image9.jpg"
+
+//project3
+
+import p3i1 from "./images/projects/project3/image1.jpg"
+import p3i2 from "./images/projects/project3/image2.jpg"
+import p3i3 from "./images/projects/project3/image3.jpg"
+import p3i4 from "./images/projects/project3/image4.jpg"
+import p3i5 from "./images/projects/project3/image5.jpg"
+import p3i6 from "./images/projects/project3/image6.jpg"
+import p3i7 from "./images/projects/project3/image7.jpg"
+import p3i8 from "./images/projects/project3/image8.jpg"
+
+//project4
+
+import p4i1 from "./images/projects/project4/image1.jpg"
+import p4i2 from "./images/projects/project4/image2.jpg"
+import p4i3 from "./images/projects/project4/image3.jpg"
+import p4i4 from "./images/projects/project4/image4.jpg"
+import p4i5 from "./images/projects/project4/image5.jpg"
+import p4i6 from "./images/projects/project4/image6.jpg"
+import p4i7 from "./images/projects/project4/image7.jpg"
+import p4i8 from "./images/projects/project4/image8.jpg"
+import p4i9 from "./images/projects/project4/image9.jpg"
+import p4i10 from "./images/projects/project4/image10.jpg"
+import p4i11 from "./images/projects/project4/image11.jpg"
+import p4i12 from "./images/projects/project4/image12.jpg"
+import p4i13 from "./images/projects/project4/image13.jpg"
+import p4i14 from "./images/projects/project4/image14.jpg"
+
+
+export { eShop, logo, profileImg, amazonImg, cyberImg, noorShop, jobJunction, realEstate, bg, bg2, 
+
+p1i1, p1i2, p1i3, p1i4, p1i5, p1i6, p1i7, p1i8, p1i9, p1i10, p1i11, 
+
+p2i1, p2i2, p2i3, p2i4, p2i5, p2i6, p2i7, p2i8, p2i9,
+
+p3i1, p3i2, p3i3, p3i4, p3i5, p3i6, p3i7, p3i8,
+
+p4i1, p4i2, p4i3, p4i4, p4i5, p4i6, p4i7, p4i8, p4i9, p4i10, p4i11, p4i12, p4i13, p4i14
+
+
+
+
 };
