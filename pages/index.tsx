e4faa6acb8f531332328dd48199e440358d37ce0
Puneeth K <kanike.puneeth@gmail.com>
2024-10-26 11:33:23 +0530

    change 33

--- pages/index.tsx
+++ pages/index.tsx
@@ -58,7 +58,7 @@ const Home: React.FC = () => {
       {loading ? (
         <Preloader />
       ) : (
-        <main className="w-full h-screen font-bodyFont bg-bodyColor text-textLight overflow-x-hidden overflow-y-scroll scrollbar scrollbar-track-textDark/20 scrollbar-thumb-textDark/60">
+        <main id="parent" className=" w-full h-screen font-bodyFont bg-bodyColor text-textLight overflow-y-scroll scrollbar scrollbar-track-textDark/20 scrollbar-thumb-textDark/60">
           <Navbar />
           <div className="w-full h-[88vh] xl:flex items-center gap-20 justify-between">
             {/* Left side animation */}
@@ -72,7 +72,7 @@ const Home: React.FC = () => {
             </motion.div>
 
             {/* Main content */}
-            <div className="h-[88vh] mx-auto p-8">
+            <div id="child" className="h-[88vh] mx-auto p-8">
               <Banner />
               <About />
               <Skills />
